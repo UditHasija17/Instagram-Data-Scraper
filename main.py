b7dc734f0b8cd8bd8f7585ee16e8330fc5fe1770
NavneetNayak <54245165+NavneetNayak@users.noreply.github.com>
2023-12-02 19:13:37 +0530

    Added function to get x recent posts as users might not want to download all posts. Added user's instagram account name as a class variable making it easier to change.

--- main.py
+++ main.py
@@ -5,29 +5,45 @@ collections.Mapping = collections.abc.Mapping
 collections.Callable = collections.abc.Callable
 from itertools import dropwhile, takewhile
 from datetime import datetime
-import instaloader
+from instaloader import Instaloader, Profile
 import csv
 
 class GetInstagramProfile():
-    def __init__(self):
-        self.ig = instaloader.Instaloader()
+
+    def __init__(self, user_username):
+        self.ig = Instaloader()
+        self.session_username = user_username
 
     def user_profile_pic(self,username):
-        self.ig = instaloader.Instaloader()
-        self.ig.load_session_from_file("episode_clusters")
+        self.ig = Instaloader()
+        self.ig.load_session_from_file(self.session_username)
         self.ig.download_profile(username, profile_pic_only=True)
         print("User Profile Pic Downloaded")
 
     def user_all_pics(self,username):
-         self.ig = instaloader.Instaloader()
-         self.ig.load_session_from_file("episode_clusters")
+         self.ig = Instaloader()
+         self.ig.load_session_from_file(self.session_username)
          self.ig.download_profile(username , profile_pic_only=False)
          print("All Post Pics Downloaded")
-         
+
+    def user_get_x_pics(self, username, x):
+        i = 1
+        self.ig = Instaloader()
+        self.ig.load_session_from_file(self.session_username)
+        user_profile = Profile.from_username(self.ig.context, username) 
+        posts = user_profile.get_posts()
+        
+        for post in posts:
+            if i > x: break
+            if post.typename != "GraphImage": continue
+            else:
+                self.ig.download_post(post, username)
+                i += 1
+
     def posts_info_with_comments(self,username):
         with open(username+'.csv', 'w', newline='', encoding='utf-8') as file:
-            self.ig = instaloader.Instaloader()
-            self.ig.load_session_from_file("episode_clusters") # change Username according to the firefox
+            self.ig = Instaloader()
+            self.ig.load_session_from_file(self.session_username) # change Username according to the firefox
             writer = csv.writer(file)
             posts = instaloader.Profile.from_username(self.ig.context, username).get_posts()
             mylist=[]
@@ -52,8 +68,8 @@ class GetInstagramProfile():
     def posts_info_without_comments(self,username):
             SINCE = datetime(2021, 6, 28)
             UNTIL = datetime(2023, 8, 12)
-            self.ig = instaloader.Instaloader()
-            self.ig.load_session_from_file("episode_clusters") # change Username according to the firefox
+            self.ig = Instaloader()
+            self.ig.load_session_from_file(self.session_username) # change Username according to the firefox
             #no = int(input('Enter No. of Posts: ')) --> Can be used for option of no. of posts we need
             with open(username+'.csv', 'w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
@@ -81,12 +97,13 @@ class GetInstagramProfile():
                 print('Post Info without comments Exported Successfully')                    
 
 if __name__=="__main__":
-    img = GetInstagramProfile()
+    img = GetInstagramProfile("prosauce2k")
     user = input("Enter Username: ")
     print("1.Print Profile Pic")
     print("2.Download Post pics")
     print("3.Get All post Data with Picture URL")
-    print("4.Get All post Data with Comments\n")
+    print("4.Get All post Data with Comments")
+    print("5.Get top x'%' of post Data with Picture URL")
     ip = int(input("Enter your choice: "))
     if ip ==1:
         img.user_profile_pic(user)
@@ -96,6 +113,9 @@ if __name__=="__main__":
         img.posts_info_without_comments(user)
     elif ip==4:
         img.posts_info_with_comments(user)
+    elif ip==5:
+        x = int(input("Enter number of Post pics to download: "))
+        img.user_get_x_pics(user, x)
     else:
         print('Invalid Option!')
     
